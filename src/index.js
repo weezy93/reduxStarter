a186b1db73e0c4ced8da0fa68c35d821ecc985b9
Louisa Reese <louisa.e.reese@gmail.com>
2016-04-23 12:43:46 -0600

    adding search bar component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// parent component, host entire youtube client
+// line 3, if there is a property on React called component, take it off and name it component to use - deconstructed variables
+import React, { Component } from 'react';
+import ReactDom from 'react-dom'; // helper method to attach components to DOM
+import SearchBar from './components/SearchBar';
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+  render () { // short syntax to create function
+    // render can only return one element, so everything must be in a parent div
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    )
+  }
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// ABOVE creates component, BELOW attaches to DOM
+// first argument is component, second argument is anchor element on DOM
+ReactDom.render(<App/>, document.querySelector('.container'));
